5dcd352c70d4f5fd987229a884c9910f43a14f80
Sumer Kohli <sumer.kohli@berkeley.edu>
2019-09-03 20:09:37 -0700

    build

---
+++ build/precache-manifest.3ed7219fe1d625ada52ccd7e3bf7a5f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8817daeb8b59a63da7ec72d66de723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14ee574b5112df4f0516",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a1d111960065a96599d4",
+    "url": "/static/css/main.5b29f858.chunk.css"
+  },
+  {
+    "revision": "14ee574b5112df4f0516",
+    "url": "/static/js/2.1f95b698.chunk.js"
+  },
+  {
+    "revision": "a1d111960065a96599d4",
+    "url": "/static/js/main.4ebc11cf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "86ffa05a693dccaaf0b5cc77730fde0c",
+    "url": "/static/media/ok-logo.86ffa05a.svg"
+  }
+]);
